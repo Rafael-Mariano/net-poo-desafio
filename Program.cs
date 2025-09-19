c958e102e9e52300d615829feb337ddb65c833f3
Rafael Mariano <rafaelmariano312@gmail.com>
2025-09-19 10:55:43 -0300

    Objetivo da proposta atingida...

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia:");
+Nokia n = new Nokia(numero: "1234", modelo: "g13", imei: "142536", memoria: 256);
+n.ReceberLigacao();
+n.Ligar();
+n.InstalarAplicativo(nomeApp: "whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone:");
+Iphone i = new Iphone(numero: "4321", modelo: "SE", imei: "142356", memoria: 128);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo(nomeApp: "calculadora");
